f02cbdaea92f72cfad71082f03db42eee5002ee4
vshantam <vshantam@yahoo.com>
2019-01-15 21:33:40 +0530

    updated

--- build_vocab.py
+++ build_vocab.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
 	#Creating Parser objects
 	parser = argparse.ArgumentParser()
 
-	parser.add_argument('--caption_path', type=str, default='data/annotations/captions_train2014.json', help='path for train annotation file')
+	parser.add_argument('--caption_path', type=str, default='data/annotations/captions_train2017.json', help='path for train annotation file')
 	parser.add_argument('--vocab_path', type=str, default='./data/vocab.pkl', help='path for saving vocabulary wrapper')
 	parser.add_argument('--threshold', type=int, default=4, help='minimum word count threshold')
 
--- resize.py
+++ resize.py
@@ -44,8 +44,8 @@ if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser()
 
-	parser.add_argument('--image_dir', type=str, default='./data/train2014/', help="directory for train images")
-	parser.add_argument('--output_dir', type=str, default='./data/resized2014/', help="directory for saving resized images")
+	parser.add_argument('--image_dir', type=str, default='./data/train2017/', help="directory for train images")
+	parser.add_argument('--output_dir', type=str, default='./data/resized2017/', help="directory for saving resized images")
 	parser.add_argument('--image_size', type=int, default=256, help="size for image after processing")
 
 	args = parser.parse_args()
